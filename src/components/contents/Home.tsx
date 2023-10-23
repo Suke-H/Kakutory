9fd5c4d3d9581ad40dfe820eb4d9aaced6403bef
Suke-H <hondii1997@gmail.com>
2023-10-24 08:25:14 +0900

    画像にカーソル当てたときのアニメーション
    css側にhoverを記載
    いずれは統一させたい（Styled ComponentかTailwind）

--- src/components/contents/Home.tsx
+++ src/components/contents/Home.tsx
@@ -1,10 +1,39 @@
 /* eslint-disable react/react-in-jsx-scope */
 // import { Link } from "react-router-dom";
 // import React from 'react';
+// import { useState } from 'react';
 import strayBot from '../../assets/home/strayBot.png';
 import shikakuReversi from '../../assets/home/shikakuReversi.png';
 import chocomint from '../../assets/home/chocomint.png';
 import memory from '../../assets/home/memory.png';
+
+import '../../App.css';
+
+// interface ImageWithHoverProps {
+//     url: string;
+//     src: string;
+//     alt: string;
+//     style: React.CSSProperties;
+//   }
+  
+// const ImageWithHover: React.FC<ImageWithHoverProps> = ({ src, alt, style }) => {
+//     return (
+//       <div className="image-container">
+//         <img src={src} alt={alt} />
+//       </div>
+//     );
+// };
+
+// const [isHovered, setIsHovered] = useState(false);
+
+// const handleMouseEnter = (): void => {
+// setIsHovered(true);
+// };
+
+// const handleMouseLeave = (): void => {
+// setIsHovered(false);
+// };
+  
 
 const homeStyle: React.CSSProperties = {
     // 文字色
@@ -13,21 +42,29 @@ const homeStyle: React.CSSProperties = {
     fontWeight: 'bold',
 }
 
+
+
 const imageWrapperStyle = {
     width: '60%',   // 画面全体の60%の幅
     display: 'grid', 
     gridTemplateColumns: '1fr 1fr',  // 2つのカラムを等しく分割
     gap: '50px',  // 2つの画像の間の余白を20pxとする
     margin: '0 auto',  // 中央寄せ
+
+    // boxShadow: '0px 0px 5px 2px rgba(0, 0, 0, 0.5)'
 };
 
 const imageItemStyle = {
     width: '100%',  // 親のコンテナの幅を全て使用
     display: 'flex',
     justifyContent: 'center',  // 横方向の中央寄せ
+
+    transition: 'transform 0.3s',
+    // transform: isHovered ? 'scale(1.2)' : 'scale(1)',
 };
 
 export const Home = (): JSX.Element => {
+
     return (
         <div style={homeStyle}>
             <h1 style={{fontSize: '40px'}}>GAMES</h1>
@@ -37,7 +74,14 @@ export const Home = (): JSX.Element => {
 
             <div style={imageWrapperStyle}>
                 <a href="https://unityroom.com/games/stray_bot">
-                    <img src={strayBot} alt="STRAY BOT" style={imageItemStyle} />
+                    <img 
+                        src={strayBot} 
+                        alt="STRAY BOT" 
+                        style={imageItemStyle} 
+                        className='img-button'
+                        // onMouseEnter={handleMouseEnter}
+                        // onMouseLeave={handleMouseLeave}
+                    />
                 </a>
                 <a href="https://unityroom.com/games/shikaku_reversi">
                     <img src={shikakuReversi} alt="シカクリバーシ"  style={imageItemStyle} />
