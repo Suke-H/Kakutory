1aa5abc22ceb5ef5c3b7bcb03c3251b9ad8333d5
Suke-H <hondii1997@gmail.com>
2023-10-24 12:41:23 +0900

    スタイル指定をcssファイルに変更

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,12 @@
 /* eslint-disable react/react-in-jsx-scope */
 // import React from 'react';
 
-const copywriteStyle: React.CSSProperties = {
-    color : '#E0E0E0',
-    position: 'absolute',
-    right: '200px',
-}
-
-const footerStyle: React.CSSProperties = {
-    position: 'relative',
-    bottom: '0',
-    width: '100%',
-    margin: '10vh 0 0 0',
-    height: '40px',
-    backgroundColor: '#585858',
-
-    // 垂直方向に中央揃え
-    display: 'flex',
-    alignItems: 'center',
-}
+import './Footer.css';
 
 export const Footer = () : JSX.Element => {
     return (
-        <footer style={footerStyle}>
-            <p style={copywriteStyle}>© 2023 かくひと</p>
+        <footer className="footer">
+            <p className="copywrite">© 2023 かくひと</p>
         </footer>
     );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,73 +1,23 @@
 /* eslint-disable react/react-in-jsx-scope */
 // import React from 'react';
 import kakutorySVG from '../assets/kakutory.svg';
-
-const widthRatio = '98%';
-
-const headerContainerStyle: React.CSSProperties = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  width: '100%',
-  marginTop: '2vh',
-}
-
-const titleStyle: React.CSSProperties = {
-    // 背景
-    backgroundColor: '#373B7A',
-    width: widthRatio,
-    height: '140px',
-
-    // 水平・垂直方向に中央揃え
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-};
-
-const separateLineStyle: React.CSSProperties = {
-  // 背景
-  backgroundColor: '#585858',
-  width: widthRatio,
-  height: '15px',
-}
-
-const menuContainerStyle: React.CSSProperties = {
-  // 背景
-  width: widthRatio,
-  height: '40px',
-  // 配置
-  display: 'flex', // 横並びに
-};
-
-const menuItemStyle: React.CSSProperties = {
-  width: '100px',
-  height: '40px',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  margin: '0 10px',
-  // 文字
-  color: '#585858',
-  fontWeight: 'bold',
-  fontSize: '24px',
-  fontFamily: 'Noto Sans JP, Impact, Meiryo,ヒラギノ角ゴ, Lato, sans-serif',
-};
+import './Header.css';
 
 export const Header = (): JSX.Element => {
   return (
-    <div style={headerContainerStyle}>
-      <div style={titleStyle}>
+    <div className="headerContainer">
+      <div className="title">
         <img src={kakutorySVG} alt="kakutory" width="360" height="60" />
       </div>
 
-      <div style={separateLineStyle}></div>
+      <div className="separateLine"></div>
 
-      <div style={menuContainerStyle}>
-      <div style={menuItemStyle}>ABOUT</div>
-      <div style={menuItemStyle}>GAMES</div>
-      <div style={menuItemStyle}>DIARY</div>
+      <div className="menuContainer">
+        <div className="menuItem">ABOUT</div>
+        <div className="menuItem">GAMES</div>
+        <div className="menuItem">DIARY</div>
       </div>
     </div>
   );
 }
+
