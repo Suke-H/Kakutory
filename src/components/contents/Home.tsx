6b66efd7874a3c1e6c25beed885854211d6efabf
Suke-H <hondii1997@gmail.com>
2023-11-30 22:47:20 +0900

    fix: Github Actionsのビルドエラー修正

--- src/components/contents/Home.tsx
+++ src/components/contents/Home.tsx
@@ -10,51 +10,9 @@ import { Diary } from './Diary';
 
 import './Home.css';
 
-interface LinkedImageProps {
-    url: string;
-    src: string;
-    alt: string;
-  }
-  
-const LinkedImage = (props: LinkedImageProps): JSX.Element => {
-    return (
-        <a href={props.url}>
-            <img src={props.src} 
-                alt={props.alt} 
-                className='imageItem'
-            />
-        </a>
-    )
-}
-
 export const Home = (): JSX.Element => {
     return (
         <div className='home'>
-            {/* <h1 className="column">GAME</h1>
-
-            <div className='imageWrapper'>
-                <LinkedImage 
-                    url="https://unityroom.com/games/stray_bot"
-                    src={strayBot}
-                    alt="STRAY BOT"
-                />
-                <LinkedImage 
-                    url="https://unityroom.com/games/shikaku_reversi"
-                    src={shikakuReversi}
-                    alt="シカクリバーシ"
-                />
-                <LinkedImage 
-                    url="https://unityroom.com/games/worship-chocomint"
-                    src={chocomint}
-                    alt="チョコミントを崇めよ！"
-                />
-                <LinkedImage 
-                    url="https://unityroom.com/games/matons_memory_journey"
-                    src={memory}
-                    alt="マトン君 記憶探しの旅"
-                />
-            </div> */}
-
             <Game/>
             <Diary/>
 
