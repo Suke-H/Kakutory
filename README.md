98463e753a7200634eccfd596e9d989de193ec6c
Suke-H <hondii1997@gmail.com>
2023-12-10 23:10:42 +0900

    write: #36 記事を2本執筆

--- README.md
+++ README.md
@@ -5,7 +5,6 @@
 かくひと([@kaku_suke_33](https://twitter.com/kaku_suke_33))がゲーム開発の活動を呟いていくホームページです。  
 目下作成中。
 
-
 <img width="600" alt="PC表示" src="./docs/thambnail_pc.png">
 <img width="300" alt="PC表示" src="./docs/thambnail_phone.png">
 
@@ -22,7 +21,7 @@ https://kakutory.com
    - React + TypeScript
    <!-- - Tailwind CSS -->
    - Material UI
-- GitHub Actions
+- GitHub Actions: 自動デプロイ
    - OIDC
 - [zenn-editor](https://github.com/zenn-dev/zenn-editor)：markdownドキュメントのhtml化
    - zenn-markdown-html
--- formatter/mdToHtml.js
+++ formatter/mdToHtml.js
@@ -7,7 +7,7 @@ const markdownToHtml = lib.default ? lib.default : lib;
 // Markdownファイルが格納されているディレクトリ
 const markdownDir = 'formatter/sources';
 // HTMLファイルを書き出すディレクトリ
-const outputDir = 'src/components/contents/diarys';
+const outputDir = 'public/diaryContents';
 
 // ディレクトリからMarkdownファイルのリストを取得
 const files = fs.readdirSync(markdownDir);
@@ -20,15 +20,15 @@ files.forEach(file => {
         console.log(outputFilePath);
 
         // 対応するHTMLファイルが存在しない場合のみ変換を実行
-        if (!fs.existsSync(outputFilePath)) {
-            // プリント
-            console.log("変換を実行");
+        // if (!fs.existsSync(outputFilePath)) {
+        //     // プリント
+        //     console.log("変換を実行");
 
             const markdown = fs.readFileSync(path.join(markdownDir, file), 'utf8');
             const html = markdownToHtml(markdown);
             fs.writeFileSync(outputFilePath, html);
             console.log(`Converted ${file} to HTML.`);
-        }   
+        // }   
 
     
     }
