512947c0c6e2a8ad8f028af4bcc87b602c545e3d
Suke-H <hondii1997@gmail.com>
2024-01-23 23:25:21 +0900

    writing: 画像を記事に添付できるかテスト
    記事内の画像が本番環境でも添付できているかテスト
    ローカルでのホットリロードおよびビルドプレビューにて確認はした。

--- src/components/contents/AllDiarySummary.ts
+++ src/components/contents/AllDiarySummary.ts
@@ -3,18 +3,25 @@ import { type DiarySummary } from './DiaryList';
 // 全日記のサマリ内容
 export const AllDiarySummary: DiarySummary[] = [
     {
+      id: 2024012301,
+      title: '東京ゲームダンジョン4にお手伝い＆遊びに行った',
+      thumbnail: '/diarySam/20240123_TGD.jpg',
+      date: '2024-01-23',
+      tags: ['レポ'],
+    },
+    {
       id: 2023121002,
       title: 'このサイトの使用技術について',
       thumbnail: '/diarySam/general.png',
       date: '2023-12-08',
-      tags: ['Web開発', 'AWS', 'React', 'TypeScript'],
+      tags: ['制作', 'Web開発'],
     },
     {
       id: 2023121001,
       title: 'ポートフォリオサイトを作りました！',
       thumbnail: '/diarySam/nikki.png',
       date: '2023-12-08',
-      tags: ['Web開発', 'AWS'],
+      tags: [`お知らせ`, 'Web開発'],
     },
 
   ];
