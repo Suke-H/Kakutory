e65727aff3ed71af19c2bef0c36113353e215aa4
Suke-H <hondii1997@gmail.com>
2024-03-11 01:01:05 +0900

    writing: 0311Wordle作ってみた

--- src/components/contents/AllDiarySummary.ts
+++ src/components/contents/AllDiarySummary.ts
@@ -3,25 +3,32 @@ import { type DiarySummary } from './DiaryList';
 // 全日記のサマリ内容
 export const AllDiarySummary: DiarySummary[] = [
     {
-      id: 2024012301,
-      title: '東京ゲームダンジョン4にお手伝い＆遊びに行った',
-      thumbnail: '/diarySam/20240123_TGD.jpg',
-      date: '2024-01-23',
-      tags: ['レポ'],
+        id: 2024031101,
+        title: 'Wordle作りました！',
+        thumbnail: '/diarySam/MyWordleProject.png',
+        date: '2024-03-11',
+        tags: ['制作', 'Web開発'],
     },
     {
-      id: 2023121002,
-      title: 'このサイトの使用技術について',
-      thumbnail: '/diarySam/general.png',
-      date: '2023-12-08',
-      tags: ['制作', 'Web開発'],
+        id: 2024012301,
+        title: '東京ゲームダンジョン4にお手伝い＆遊びに行った',
+        thumbnail: '/diarySam/20240123_TGD.jpg',
+        date: '2024-01-23',
+        tags: ['レポ'],
     },
     {
-      id: 2023121001,
-      title: 'ポートフォリオサイトを作りました！',
-      thumbnail: '/diarySam/nikki.png',
-      date: '2023-12-08',
-      tags: [`お知らせ`, 'Web開発'],
+        id: 2023121002,
+        title: 'このサイトの使用技術について',
+        thumbnail: '/diarySam/general.png',
+        date: '2023-12-08',
+        tags: ['制作', 'Web開発'],
+    },
+    {
+        id: 2023121001,
+        title: 'ポートフォリオサイトを作りました！',
+        thumbnail: '/diarySam/nikki.png',
+        date: '2023-12-08',
+        tags: [`お知らせ`, 'Web開発'],
     },
 
-  ];
+];
