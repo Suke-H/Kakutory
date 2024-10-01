fe5b49156c923e6e8c26771ab8a21ed4ddb25fa1
Suke-H <hondii1997@gmail.com>
2024-10-01 22:56:09 +0900

    :zap: feat: #73 紹介カードの作成

--- src/components/contents/Game.tsx
+++ src/components/contents/Game.tsx
@@ -3,7 +3,9 @@
 // import React from 'react';
 
 import { Heading } from '../Heading';
+import { Heading2 } from '../Heading2';
 import { type IsHomeProp } from './IsHomeProp';
+import { Typography, Link, Card, CardActionArea, CardMedia, CardContent } from '@mui/material';
 
 import './Game.css';
 import Button from '@mui/material/Button';
@@ -17,6 +19,7 @@ import Fulltank from '../../assets/game/Fulltank.png';
 import Company from '../../assets/game/Company.png';
 import kiro from '../../assets/game/kiro.png';
 import konatsu from '../../assets/game/konatsu.png';
+import TankStrike from '../../assets/game/TankStrike.png';
 
 interface LinkedImageProps {
     url: string;
@@ -35,7 +38,116 @@ const LinkedImage = (props: LinkedImageProps): JSX.Element => {
     )
 }
 
+interface GameCardProps {
+    url: string;
+    imageUrl: string;
+    title: string;
+    date: string;
+    description: string;
+}
+
+const GameCard = ({ url, imageUrl, title, date, description }: GameCardProps): JSX.Element => {
+    return (
+        <Card sx={{ width: '100%' }}>
+            <CardActionArea href={url}>
+                <CardMedia
+                    component="img"
+                    image={imageUrl}
+                    alt={title}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: 'bold' }}>
+                        {title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {date}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+};
+
 export const Game = ({ isHome }: IsHomeProp  ): JSX.Element => {
+
+    const games = [
+        {   
+            url: "https://unityroom.com/games/stray_bot", 
+            imageUrl: strayBot, 
+            title: "STRAY BOT", 
+            date: "2024-09-30", 
+            description: "最初にゲーム制作に取り組んだ作品" 
+        },
+        { 
+            url: "https://unityroom.com/games/shikaku_reversi", 
+            imageUrl: shikakuReversi,
+            title: "シカクリバーシ", 
+            date: "2024-08-15", 
+            description: "A challenging puzzle game." 
+        },
+        { 
+            url: "https://unityroom.com/games/dear_past_diary", 
+            imageUrl: konatsu, 
+            title: "こなつのえにっき", 
+            date: "2024-10-01", 
+            description: "Daily life and adventures of Konatsu." 
+        },
+        { 
+            url: "https://unityroom.com/games/tank_strike", 
+            imageUrl: TankStrike, 
+            title: "TANK STRIKE!!", 
+            date: "2024-10-01", 
+            description: "Daily life and adventures of Konatsu." 
+        },
+        
+    ];    
+    const u1wgames = [
+        {   
+            url: "https://unityroom.com/games/kaerimichi", 
+            imageUrl: kiro, 
+            title: "帰路", 
+            date: "2024-09-30", 
+            description: "Explore as a robot..." 
+        },
+        {   
+            url: "https://unityroom.com/games/dear_past_diary", 
+            imageUrl: DearPastDiary, 
+            title: "『Dear past diary, 』", 
+            date: "2024-09-30", 
+            description: "Explore as a robot..." 
+        },
+        { 
+            url: "https://unityroom.com/games/matons_memory_journey", 
+            imageUrl: memory, title: "マトン君 記憶探しの旅", 
+            date: "2024-10-01", 
+            description: "Join Maton in a quest for his lost memories." },
+        { 
+            url: "https://unityroom.com/games/worship-chocomint", 
+            imageUrl: chocomint,
+            title: "チョコミントを崇めよ！", 
+            date: "2024-08-15", 
+            description: "A challenging puzzle game." 
+        },
+    
+        
+        { 
+            url: "https://unityroom.com/games/full_tank_by_kakusuke", 
+            imageUrl: Fulltank, 
+            title: "FULL TANK", 
+            date: "2024-10-01", 
+            description: "Fuel up and roll out on an explosive adventure." 
+        },
+        { 
+            url: "https://unityroom.com/games/company-re-innovation", 
+            imageUrl: Company, 
+            title: "カンパニーRe:イノベーション", 
+            date: "2024-10-01", 
+            description: "Revolutionize the company from the inside out." 
+        },
+    ];
 
     // Home の場合の内容
   if (isHome) {
@@ -86,62 +198,43 @@ export const Game = ({ isHome }: IsHomeProp  ): JSX.Element => {
   // Home ではない場合の内容
   return (
     <div className='game'>
-      <Heading text={"GAME"} />
+        <Heading text={"GAME"} />
 
-      <Heading text={"GAME"} />
+        <Heading2 text={"リリース作品"} />
+        <Typography gutterBottom component="div" 
+            sx={{
+                marginBottom: "40px",
+                marginLeft: "10%",
+                marginRight: "10%",
+                fontSize: { xs: '12px', md: '16px' }
+                }} >
+                {"2022年の『STRAY BOT』からゲーム制作をはじめました（こっちをもっと増やしたいな...）"}
+        </Typography>
 
       <div className='imageWrapper'>
-        <LinkedImage 
-          url="https://unityroom.com/games/stray_bot"
-          src={strayBot}
-          alt="STRAY BOT"
-        />
-        <LinkedImage 
-          url="https://unityroom.com/games/shikaku_reversi"
-          src={shikakuReversi}
-          alt="シカクリバーシ"
-        />
+      {games.map(game => (
+                    <GameCard key={game.url} {...game} />
+            ))}
       </div>
 
-      <Heading text={"Unity1Week作品"} />
+        <Heading2 text={"Unity1Week作品"} />
+        <Typography gutterBottom component="div" 
+            sx={{
+                marginBottom: "40px",
+                marginLeft: "10%",
+                marginRight: "10%",
+                fontSize: { xs: '12px', md: '16px' }
+                }} >
+                <Link href={"https://unityroom.com/unity1weeks"} sx={{ color: '#1976d2', textDecoration: 'none' }}>
+                    {"「Unity1週間ゲームジャム」"}
+                </Link>
+                {"への参加作品です。2022年9月『ふる』から参加をはじめ、計6回参加しました。"}
+        </Typography>
 
       <div className='imageWrapper'>
-        <LinkedImage 
-          url="https://unityroom.com/games/dear_past_diary"
-          src={kiro}
-          alt="帰路"
-        />
-        <LinkedImage 
-          url="https://unityroom.com/games/dear_past_diary"
-          src={konatsu}
-          alt="こなつのえにっき"
-        />
-        <LinkedImage 
-          url="https://unityroom.com/games/dear_past_diary"
-          src={DearPastDiary}
-          alt="『Dear past diary, 』"
-        />
-        <LinkedImage 
-          url="https://unityroom.com/games/worship-chocomint"
-          src={chocomint}
-          alt="チョコミントを崇めよ！"
-        />
-        <LinkedImage 
-          url="https://unityroom.com/games/matons_memory_journey"
-          src={memory}
-          alt="マトン君 記憶探しの旅"
-        />
-        <LinkedImage 
-          url="https://unityroom.com/games/matons_memory_journey"
-          src={Fulltank}
-          alt="FULL TANK"
-        />
-        <LinkedImage 
-          url="https://unityroom.com/games/matons_memory_journey"
-          src={Company}
-          alt="カンパニーRe:イノベーション"
-        />
-        
+            {u1wgames.map(game => (
+                    <GameCard key={game.url} {...game} />
+            ))}
         
       </div>
     </div>
