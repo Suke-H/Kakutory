34dbcdaccfdd9232a7c1a4d0869c901179ede6b7
Suke-H <hondii1997@gmail.com>
2024-12-26 22:15:39 +0900

    :zap: feat: #81 Privacyページを作成・ルーティング

---
+++ src/components/contents/Privacy.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/react-in-jsx-scope */
+// import { Link } from "react-router-dom";
+// import React from 'react';
+
+import { Heading } from '../Heading';
+import { PrivacyContent } from './PrivacyContent';
+
+import './Privacy.css';
+// import Button from '@mui/material/Button';
+
+export const Privacy = (): JSX.Element => {
+    return (
+        <div className='privacy'>
+            <Heading text={"プライバシーポリシー"} />
+            
+            <PrivacyContent />
+        </div>
+    );
+}
